2aac5ec142f5b724f765f67827adc30d0a3290c7
Bright <melvinbright101@gmail.com>
2025-02-18 16:25:49 +0100

    modified

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -13,6 +13,5 @@ class BookAdmin(admin.ModelAdmin):
     # Enable search functionality for title and author
     search_fields = ('title', 'author')
 
-
-# Register the Book model
-admin.site.register(Book)
+# Register the Book model with the custom admin class
+admin.site.register(Book, BookAdmin)
