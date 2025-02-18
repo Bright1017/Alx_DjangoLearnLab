80e7d4a5ecb45a760589d132dbe53bdf44a8bb13
Bright <melvinbright101@gmail.com>
2025-02-18 16:01:25 +0100

    modified

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -8,3 +8,13 @@ class Book(models.Model):
     
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
+    
+    # bookshelf/admin.py
+from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
