c87ed2e38cffd37f3a1ecde5b3a5628b4a451635
Bright <melvinbright101@gmail.com>
2025-02-26 12:11:34 +0100

    modified view.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.views import LoginView, LogoutView # Class Based View
 from django.contrib.auth import login, logout # Function Based view
 from .models import Library
 from .models import Book
+from django.contrib.auth.decorators import user_passes_test, login_required
+from django.http import HttpResponseForbidden
 # Create your views here.
 
 # Function Base View For Listing all books
@@ -29,7 +31,7 @@ class SignupView(CreateView):
     template_name = "register.html"
     
 
-class CustomLoginView(LoginView):
+# class CustomLoginView(LoginView):
     template_name = 'login.html'
     
 class CustomLogoutView(LogoutView):
@@ -60,4 +62,34 @@ def login_view(request):
     
     else:
         form = AuthenticationForm()
-        return render(request, "registration_app/login.html", {"form": form})
+        return render(request, "registration_app/login.html", {"form": form})  
+    
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
