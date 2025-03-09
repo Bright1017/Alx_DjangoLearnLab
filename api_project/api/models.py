14a7662e5179a182a8df5ce4f9a79501406d5212
Bright <melvinbright101@gmail.com>
2025-03-09 13:43:53 +0100

    modified files

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=50)
-    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=15, null=False, blank=False)
+    author = models.CharField(max_length=20, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.id}" 
