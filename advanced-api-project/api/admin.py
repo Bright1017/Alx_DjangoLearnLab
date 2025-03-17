5551b3b35740c50c33810f63f8e806e4c49067c3
Bright1017 <melvinbright101@gmail.com>
2025-03-17 14:13:38 +0100

    Writing Unit Tests for Django REST Framework APIs.

--- advanced-api-project/api/admin.py
+++ advanced-api-project/api/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Author, Book
+from .models import Book, Author
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publication_year']
+    list_filter = ['title', 'author', 'publication_year']
+    search_fields = ['title']
 
-admin.site.register(Author)
-admin.site.register(Book)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['name']
+    search_fields = ['name']
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,19 +1,24 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
-
 class Author(models.Model):
-    """Represents an author with a name. Each author can have multiple books."""
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50, null=False, blank=False)
 
     def __str__(self):
         return self.name
 
 class Book(models.Model):
-    """Represents a book with title, publication year, and a foreign key to Author."""
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=50, null=False, blank=False)
     publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="book")
+
+    def clean(self):
+        """Ensure publication_year is not in the future"""
+        from datetime import datetime
+        current_year = datetime.now().year
+        if self.publication_year > current_year:
+            raise ValidationError({'publication_year': 'Publication year cannot be in the future.'})
 
     def __str__(self):
         return self.title
