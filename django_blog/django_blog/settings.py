da3d994a571b6eef26cf777b5beea9a1ad16dd24
Bright1017 <melvinbright101@gmail.com>
2025-03-19 12:44:11 +0100

    modified settings.py file in django_blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,13 +78,17 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'Bright',
+        'PASSWORD': 'bright101',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -121,14 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = 'static/'
 
+# Additional locations for static files
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',           # Project-wide static files
+    BASE_DIR / 'blog/static/blog',
+ ] # App-specific static files
+
 # For production (collectstatic command)
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# # Additional locations for static files
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',           # Project-wide static files
-#     BASE_DIR / 'blog/static/blog',
-#  ] # App-specific static files
 
 # user-uploaded contect
 MEDIA_URL = '/media/'
