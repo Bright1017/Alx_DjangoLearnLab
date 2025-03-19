a77bfd0f6d9234f6680dd4a4810e3250814e86eb
Bright1017 <melvinbright101@gmail.com>
2025-03-19 15:41:28 +0100

    modified files

--- django_blog/blog/tests.py
+++ django_blog/blog/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your tests here.
+class AuthTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass123')
+
+    # def test_login_view(self):
+    #     response = self.client.post(reverse('login'), {'username': 'testuser', 'password': 'testpass123'})
+    #     self.assertEqual(response.status_code, 302)  # Redirect after login
+
+    def test_profile_access_unauthorized(self):
+        response = self.client.get(reverse('profile'))
+        self.assertRedirects(response, '/login/?next=/profile/')
+
+    def test_csrf_token_present(self):
+        response = self.client.get(reverse('register'))
+        self.assertContains(response, 'csrfmiddlewaretoken')
